d419f9ec5ca0df332e0135e5d958342964d5759b
routetodotnet <routetodotnet@gmail.com>
2020-08-22 23:36:29 -0400

    Add project files.

---
+++ OrdersCoreApi/Controllers/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrdersCoreApi.Models;
+using OrdersCoreApi.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrdersCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+        public IOrderRepository _repo;
+        public OrderController(IOrderRepository repo)
+        {
+            _repo = repo;
+        
+        }
+        // GET: api/<OrderController>
+        [HttpGet]
+        public IEnumerable<Order> Get()
+        {
+            return _repo.GetAllOrders();
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrderController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ OrdersCoreApi/Repositories/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using OrdersCoreApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersCoreApi.Repositories
+{
+
+    public interface IOrderRepository
+    {
+
+        IEnumerable<Order> GetAllOrders();
+
+
+    }
+
+    public class OrderRepository:IOrderRepository
+    {
+        public IEnumerable<Order> GetAllOrders()
+        {
+            var orders = new List<Order>()
+            {
+                new Order(){ id=101,name ="TV",desc="Samsung"},
+                new Order(){ id=102,name ="IPhone",desc="Iphone11"},
+                new Order(){ id=103,name ="Car",desc="Tesla"}
+
+
+            };
+
+            return orders;
+
+        }
+    }
+}
